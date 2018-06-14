18dc577577447211899e883d33ab0907ab300aed
zmxxx <1214026832@qq.com>
2018-06-14 15:12:18 +0800

    realize Class LinkStack

---
+++ src/org/stack/LinkStack.java
@@ -0,0 +1,62 @@
+package org.stack;
+
+import org.list.LinearList;
+import org.list.Node;
+
+public class LinkStack extends Stack {
+    private Node first;
+    public LinkStack()
+    {
+        first = null;
+    }
+    @Override
+    public boolean isEmpty() {
+        return first==null;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public Object top() {
+        if(isEmpty())
+        {
+            return "Empty Stack";
+        }
+        return first.getElement();
+    }
+
+    @Override
+    public boolean push(Object x) {
+        Node p = new Node();
+        p.setElement(x);
+        if(isEmpty())
+        {
+            first = p;
+        }
+        else
+        {
+            p.setNext(first);
+            first = p;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean pop() {
+        if(isEmpty())
+        {
+            System.out.println("Empty Stack");
+            return false;
+        }
+        first = first.getNext();
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        first = null;
+    }
+}
---
+++ src/org/stack/TestLinkStack.java
@@ -0,0 +1,8 @@
+package org.stack;
+
+public class TestLinkStack {
+    public static void main(String[] args) {
+        LinkStack ls = new LinkStack();
+        TestClass.testStack(ls);
+    }
+}
